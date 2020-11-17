662e08d8afff2ce7017a28ebbc8439e9cbfc28c1
Facundo Decena <facundo.decena@gmail.com>
2020-11-16 21:20:12 -0300

    Rewriter and not so pretty printer

--- main.py
+++ main.py
@@ -2,21 +2,20 @@ import sys
 from antlr4 import *
 from CLexer import CLexer
 from CParser import CParser
-# from antlr4.TokenStreamRewriter import *
+from antlr4.TokenStreamRewriter import *
 from os import walk
 
 from Functions import Functions
 from constant import *
 
 
-
 def find_c_files(root):
-    f = []
+    files = []
     for (dirpath, _, filenames) in walk(top=root):
         for filename in filenames:
             if filename.endswith('.c') or filename.endswith('.h'):
-                f.append(dirpath + "/" + filename)
-    return f
+                files.append(dirpath + "/" + filename)
+    return files
 
 
 def main(argv):
@@ -30,7 +29,7 @@ def main(argv):
             lexer = CLexer(input_stream)
             stream = CommonTokenStream(lexer)
             stream.fill()
-            # rewriter = TokenStreamRewriter(tokens=stream)
+            rewriter = TokenStreamRewriter(tokens=stream)
             parser = CParser(stream)
             tree = parser.compilationUnit()
             function = Functions()
@@ -41,9 +40,15 @@ def main(argv):
                 for function in function.functions:
                     if function[FUNCTION_LINES] > MAX_SAFE_LINES:
                         print("Atencion: Esta funcion tiene más de", MAX_SAFE_LINES, "lineas, puede ser posible "
-                              "dividirla en subfunciones")
-                        # rewriter.insertBefore("default", f[1], "// Este archivo contiene una funcion a mejorar \n")
-                        # print(rewriter.getDefaultText())
+                                                                                     "dividirla en subfunciones")
+                        rewriter.insertBefore("default", function[FUNCTION_START], "/* Es posible que esta funcion "
+                                                                                   "pueda ser refactorizada */ \n")
+                        ugly_code = rewriter.getText("default", 0, 9000)
+                        almost_pretty_code = ugly_code.replace(";", ";\n")
+                        almost_pretty_code = almost_pretty_code.replace("{", "{\n")
+                        almost_pretty_code = almost_pretty_code.replace("}", "}\n")
+                        print(almost_pretty_code)
+
                     print("\t", function)
             else:
                 print("No se encontraron funciones")
